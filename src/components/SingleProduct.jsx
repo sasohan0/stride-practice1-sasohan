f3bdae8c3e69ae33a2a3ef42adec7726174a63a1
Solih Ahmad Sohan <sasohanme@gmail.com>
2024-05-26 00:21:53 +0600

    size changed of description

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -5,15 +5,19 @@ const SingleProduct = ({ shoe }) => {
   const { id, title, brand, price, description, image_url } = shoe;
 
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
+    <div className="card w-96 h-auto bg-base-100 shadow-xl">
+      <figure className="bg-base-200">
         <img src={image_url} alt="Shoes" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
         <h3 className="text-xl font-semibold">{brand}</h3>
         <h3 className="text-xl font-semibold">{price}</h3>
-        <p>{description}</p>
+        <p className="w-full h-20">
+          {description.length > 127
+            ? description.slice(0, 127) + "..."
+            : description}
+        </p>
         <div className="card-actions justify-end">
           <button className="btn btn-primary">
             <Link to={`/products/${id}`}>See details</Link>
--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -7,10 +7,10 @@ const Products = ({ data }) => {
     <div>
       <h1 className="my-8 text-2xl font-bold text-center">Our Products</h1>
 
-      <div className="flex gap-2 px-6 justify-center items-center ">
+      <div className="flex flex-wrap gap-7 px-6 justify-center items-center ">
         {
           // eslint-disable-next-line react/prop-types
-          data.slice(0, 3).map((shoe) => (
+          data.map((shoe) => (
             <SingleProduct key={shoe.id} shoe={shoe} />
           ))
         }
